635efd47ee4d5f71f768630779da8a24a0c46c39
kaial <kaial@KAIRALLAN>
2025-12-25 21:23:26 +0000

    Add project files.

---
+++ TheGoldenCombatManager/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheGoldenCombatManager
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
